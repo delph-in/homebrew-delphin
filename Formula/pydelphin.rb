ca4fb2f2a125e35f67471a5ebe421caab28010a4
T.J. Trimble <trimblet@me.com>
2021-04-22 22:52:40 -0700

    Fix and clean-up pydelphin formula

--- Formula/pydelphin.rb
+++ Formula/pydelphin.rb
@@ -1,14 +1,14 @@
 class Pydelphin < Formula
   include Language::Python::Virtualenv
-  
+
   desc "Python libraries for DELPH-IN"
   homepage "https://github.com/delph-in/pydelphin"
   url "https://github.com/delph-in/pydelphin/archive/v1.2.3.tar.gz"
   sha256 "273b1429c91c7579fccc5fa7cfe9a5fe2185fb4cb65d73823ccf964e8db751de"
 
+  depends_on "delph-in/delphin/ace"
   depends_on "python"
-  depends_on "dantiston/delphin/ace" # => :recommended
-  
+
   resource "Penman" do
     url "https://files.pythonhosted.org/packages/a2/3d/50a8b1c7d6632fa793b406485d9ed0afa8683442d0aa83462c0ab7b67560/Penman-0.9.1.tar.gz"
     sha256 "ec1b0071948563d59004e11bd5c0e4696b4ceba95bf946367a4ba2b99bcd4f42"
@@ -34,36 +34,37 @@ class Pydelphin < Formula
       url "https://github.com/dantiston/delphin-tiniest-grammar/archive/v1.0.0.tar.gz"
       sha256 "d24542f56fc5dbb63ba761f3bc292c5e01178e9dbefa024bc00ba9a04cbc210b"
     end
-    
+
     testpath.install resource("tiniest-grammar")
     # Write TSDB info
     (testpath/"sentences.txt").write <<~EOS
-    i-wf@i-input@i-author
-    1@n1 iv@author
-    2@iv n1@author
+      i-wf@i-input@i-author
+      1@n1 iv@author
+      2@iv n1@author
     EOS
-    
+
     (testpath/"relations.txt").write <<~EOS
-    item:
-      i-input :string
-      i-wf :integer
-      i-author :string
+      item:
+        i-input :string
+        i-wf :integer
+        i-author :string
 
-    run:
-      run-id :integer :key
+      run:
+        run-id :integer :key
 
-    parse:
-      parse-id :integer :key
-      run-id :integer :key
-      i-id :integer :key
+      parse:
+        parse-id :integer :key
+        run-id :integer :key
+        i-id :integer :key
     EOS
 
     # Make the profile
-    system bin/"delphin", "mkprof", testpath/"tests", "--delimiter=\"@\"", "--relations", "relations.txt", "--input", "sentences.txt"
+    system bin/"delphin", "mkprof", testpath/"tests", "--delimiter=\"@\"", "--relations", "relations.txt", "--input",
+"sentences.txt"
     assert_predicate testpath/"tests"/"item", :exist?
 
     # Compile the grammar & process profile
-    system Formula["dantiston/delphin/ace"].bin/"ace", "-g", "config.tdl", "-G", testpath/"grammar.dat"
+    system Formula["delph-in/delphin/ace"].bin/"ace", "-g", "config.tdl", "-G", testpath/"grammar.dat"
     system bin/"delphin", "process", "-g", testpath/"grammar.dat", testpath/"tests"
 
     assert_predicate testpath/"tests"/"run", :exist?
